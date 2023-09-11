8a3593ba4c4a7be2246da204c8a0d2323b644051
angusssyyy <angusw2026@student.cis.edu.hk>
2023-09-11 13:39:33 +0800

    1.2

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String [] args) {
+
+
         System.out.println("hello world");
         System.out.println("hi");
+        System.out.println("a");
+
+
     }
-
-
 }
